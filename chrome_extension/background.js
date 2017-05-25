fac86982ef47dfd30193afdebf0d1e89ae89b0a1
Vincent Lidou <vincent.lidou@hootsuite.com>
2017-05-25 06:23:18 -0700

    page script

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,13 +1,8 @@
-const APP_PORT = 8081;
-var apiUrl = 'http://localhost:' + APP_PORT + '/';
-
-var accountInfo;
 var rules = [];
 var functionsText;
 
 var dep_page = {};
 var dep_jquery = {};
-var counter = 0;
 
 function updateGlobals(config) {
     try {
@@ -16,29 +11,32 @@ function updateGlobals(config) {
     } catch (ex) { }
 }
 
-/*
-chrome.browserAction.onClicked.addListener(function(activeTab){
-    chrome.tabs.getUrl('popup.html');
+// A generic onclick callback function.
+function genericOnClick(info, tab) {
+    console.log("item " + info.menuItemId + " was clicked");
+    console.log("info: " + JSON.stringify(info));
+    console.log("tab: " + JSON.stringify(tab));
+    chrome.tabs.sendMessage(tab.id, { greeting: "Hi from background script" });
+}
+
+var title = "menu item";
+var contextMenuId = chrome.contextMenus.create({ "title": title, "contexts": ["all"], "onclick": genericOnClick });
+
+console.log('____233');
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.runtime.openOptionsPage();
 });
-*/
-
-
-chrome.browserAction.onClicked.addListener(function(tab) { 
-chrome.runtime.openOptionsPage();
-});
-
 
 chrome.storage.sync.get('config', function (items) {
-    console.log('loadingItems: ', items);
     config = items['config'];
 
     if (config === undefined)
         config = {};
 
     updateGlobals(config);
-    console.log('loadingConfig: ', config);
-
     chrome.tabs.onUpdated.addListener(injectDependenciesAfterPageLoaded);
+    chrome.tabs.onActivated.addListener(afterActivePageChanged);
 
     chrome.runtime.onMessage.addListener(
         function (request, sender, sendResponse) {
@@ -71,22 +69,32 @@ function isMatchRule(str, rule) {
 }
 
 function afterTabUpdated(tabId) {
+    console.log("__afterTabUpdated", tabId);
     chrome.tabs.get(tabId, function (tab) {
         console.log('rules BEGIN');
 
+        chrome.contextMenus.update(contextMenuId, { "enabled": false });
         rules.some(function (rule) {
             console.log('rule url:' + rule.url + ' current url:' + tab.url);
             if (isMatchRule(tab.url, rule.url)) {
                 console.log('match!: ' + JSON.stringify(rule));
 
-                if (dep_jquery[tab.id] === tab.url && dep_page[tab.id] === tab.url)
+                if (rule.contextMenu) {
+                    chrome.contextMenus.update(contextMenuId, { "enabled": true });
+                }
+
+                if (dep_jquery[tab.id] === tab.url && dep_page[tab.id] === tab.url) {
+                    rule.isSetChromeTitle = true;
                     chrome.tabs.sendMessage(tab.id, rule);
 
-                else
+                    var rule2 = {};
+                    rule2.isRevealMIDs = true;
+                    chrome.tabs.sendMessage(tab.id, rule2);
+                } else
                     setTimeout(function () {
                         console.log('___ delay restart send message');
                         afterTabUpdated(tabId);
-                    }, 50);
+                    }, 250);
                 return true;
             }
         });
@@ -99,21 +107,57 @@ function injectDependenciesAfterPageLoaded(tabId, changeInfo, tab) {
     if (tab.url.startsWith("chrome://"))
         return;
 
-    console.log('changeInfo.status: ' + changeInfo.status);
+    console.log('changeInfo.status: ' + changeInfo);
     if (changeInfo.status === "loading" || changeInfo.status === "complete") {
-        //if (changeInfo.status === "loading") {
-        chrome.tabs.executeScript(tabId, { file: "jquery-3.1.1.min.js" }, function () {
-            dep_jquery[tabId] = tab.url;
-            chrome.tabs.executeScript(tabId, { file: "page.js" }, function () {
-                dep_page[tabId] = tab.url;
+        injectScriptsIntoTab(tabId, true);
+    }
+}
 
-                chrome.tabs.executeScript(tabId, { code: functionsText }, function () {
-                    afterTabUpdated(tabId);
+function injectScriptsIntoTab(tabId, forceInject) {
+    console.log("injectScriptsIntoTab:", tabId);
+    chrome.tabs.get(tabId, function (tab) {
+        console.log("tab:", tab);
+        console.log("tab.url:", tab.url);
+        if (tab.url.startsWith('chrome://'))
+            return;
+
+        injectJqueryIfNeeded(tab,forceInject, function () {
+            injectPageJsIfNeeded(tab,forceInject, function () {
+                chrome.tabs.executeScript(tab.id, { code: functionsText }, function () {
+                    chrome.tabs.insertCSS(tab.id, {
+                        file: "styles.css"
+                    }, function () {
+                        afterTabUpdated(tab.id);
+                    });
+
                 });
-
             });
         });
+    });
+}
+
+function injectJqueryIfNeeded(tab, forceInject, callback) {
+    if (forceInject ||dep_jquery[tab.id] !== tab.url) {
+        chrome.tabs.executeScript(tab.id, { file: "jquery-3.1.1.min.js" }, function () { callback(); });
+        dep_jquery[tab.id] = tab.url;
+        console.log('dep_jquery:[%s] = %s', tab.id, tab.url);
+    } else {
+        callback();
     }
+}
+
+function injectPageJsIfNeeded(tab, forceInject, callback) {
+    if (forceInject || dep_page[tab.id] !== tab.url) {
+        chrome.tabs.executeScript(tab.id, { file: "page.js" }, function () { callback(); });
+        dep_page[tab.id] = tab.url;
+        console.log('dep_page:[%s] = %s', tab.id, tab.url);
+    } else {
+        callback();
+    }
+}
+
+function afterActivePageChanged(activeInfo) {
+    injectScriptsIntoTab(activeInfo.tabId, false);
 }
 
 /*
--- chrome_extension/page.js
+++ chrome_extension/page.js
@@ -3,10 +3,106 @@ if (!document.hasRun) {
     console.log('onMessage listener added');
     chrome.runtime.onMessage.addListener(messageListener);
     document.hasRun = true;
+
+    var divTag = document.createElement('div');
+    divTag.className = 'popup';
+    divTag.setAttribute('data-popup', 'popup-1');
+    document.body.appendChild(divTag);
+
+    var divInnerTag = document.createElement('div');
+    divInnerTag.className = 'popup-inner';
+    divTag.appendChild(divInnerTag);
+
+    // contains text..
+    var divContentsTag = document.createElement('div');
+    divInnerTag.appendChild(divContentsTag);
+
+    var aTag = document.createElement('a');
+    aTag.className = 'popup-close';
+    aTag.id = 'a123456789';
+    aTag.href = '#';
+    aTag.innerText = 'x'
+    divInnerTag.appendChild(aTag);
+
+    $(function () {
+        var elem = $('#a123456789');
+        console.log('elem:', elem);
+        $('#a123456789').on('click', function (e) {
+            //alert('clicked!');
+            $('[data-popup]').fadeOut(350);
+            e.preventDefault();
+        });
+
+        console.log('___ popup handlers intialized 2');
+    });
+
+    console.log('texarea created!')
+} else {
+    console.log('__ already ran!');
 }
 
 // function that waits for commands from background extension worker
 function messageListener(request, sender, sendResponse) {
+    console.log('messageListener');
+
+    if (request.isSetChromeTitle) {
+        handleOverrideWebTitle(request);
+
+    } else if (request.isRevealMIDs) {
+        handleShowMIDs();
+
+    } else {
+        console.log('______ showPopup');
+        showPopup();
+    }
+}
+
+function showPopup() {
+    //alert('showPopup5');
+    var popup = $('[data-popup]');
+
+    var pTag = document.createElement('p');
+    pTag.innerText = "Donec in volutpat nisi. In quam lectus, aliquet rhoncus cursus a, congue et arcu. Vestibulum tincidunt neque id nisi pulvinar aliquam. Nulla luctus luctus ipsum at ultricies. Nullam nec velit dui. Nullam sem eros, pulvinar sed pellentesque ac, feugiat et turpis. Donec gravida ipsum cursus massa malesuada tincidunt. Nullam finibus nunc mauris, quis semper neque ultrices in. Ut ac risus eget eros imperdiet posuere nec eu lectus.";
+    divContentsTag.appendChild(pTag);
+
+    console.log('___ popup2:', popup);
+    popup.fadeIn(350);
+}
+
+function handleShowMIDs() {
+
+    setTimeout(function () {
+        $('#1234567890poiuy').remove();
+
+        var elemDiv = document.createElement('textarea');
+        elemDiv.id = "1234567890poiuy";
+        // refresh button        
+        elemDiv.style.cssText = 'position:absolute;margin-left:50px;bottom:0;width:100%;height:100px;z-index:1000;';
+
+
+        var text = ''
+        $('div[mid]').each(function (index) {
+
+            console.log($(this));
+            var seqNumber = $(this).find('._sequenceNumber')
+            sequenceNumber = '';
+            if (seqNumber.length)
+                sequenceNumber = seqNumber.attr('value');
+
+            text += $(this).attr('mid') + '[' + sequenceNumber + ']' + ': ' + $(this).text().replace(/\s\s+/g, ' ') + '\n';
+        });
+
+        elemDiv.innerHTML = text;
+
+        if (text !== '') {
+            document.body.appendChild(elemDiv);
+        }
+
+    }, 3000);
+
+}
+
+function handleOverrideWebTitle(request) {
     try {
         console.log('incoming command: ' + JSON.stringify(request));
 
